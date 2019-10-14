8bca652d548022461e78a0f67290c4445516d795
ningyunhan <ningyunhan19940829@gmail.com>
2019-10-13 23:00:41 -0700

    minor fix

--- index.js
+++ index.js
@@ -2,8 +2,65 @@ var RgbToHex = null;
 
 (function($){
     RgbToHex = {
-        init: function(){
+        form: $("#rgb-2-hex-form"),
+        convertBtn: $("#rgb-2-hex-form button[data-type='convert']"),
+        result: $("#result-container .result"),
+        resultDemo: $("#result-container .result-demo"),
+        formValidator: null,
 
+        onClickConvertBtn: function() {
+            var self = this;
+            this.convertBtn.off("click");
+            this.convertBtn.on("click", function(){
+                if(self.form.valid()) {
+                    var result = "#";
+                    $.map(self.form.find("input"), function(input){
+                        var target = $(input);
+                        var res = (target.val() * 1).toString(16);
+                        if(res.length === 1) {
+                            res = "0" + res;
+                        }
+
+
+                        result += res;
+                    });
+
+                    self.result.empty().text(result);
+                    self.resultDemo.css({
+                        background: result
+                    });
+
+                    self.formValidator.resetForm();
+                }
+            })
+        },
+
+        initForm: function() {
+            this.formValidator = this.form.validate({
+                rules: {
+                    r: {
+                        min: 0,
+                        max: 255,
+                        step: 1
+                    },
+                    g: {
+                        min: 0,
+                        max: 255,
+                        step: 1
+                    },
+                    b: {
+                        min: 0,
+                        max: 255,
+                        step: 1
+                    }
+                }
+            })
+        },
+
+        init: function(){
+            this.initForm();
+
+            this.onClickConvertBtn();
         }
     };
 
